db2809b61e021397bd24987b75180a28786e0323
jiumi <jiumilove@163.com>
2018-07-20 23:42:07 +0800

    前端样式完成

---
+++ temp/pages/img/img.js
@@ -0,0 +1,66 @@
+// pages/person/person.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    imgsrc:'http://pic.90sjimg.com/back_pic/qk/back_origin_pic/00/02/28/ded5cf158629ff7686dcef434ce5b1d7.jpg'
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
